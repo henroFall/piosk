cf3b782d9c5e25ae04f4bc4a956b1f00dded1317
henroFall <henryfell@gmail.com>
2024-07-08 13:57:15 -0500

    Update browser.sh

--- browser.sh
+++ browser.sh
@@ -1,9 +1,21 @@
-chromium-browser \
-  $(jq -r '.urls | map(.url) | join(" ")' ~/piosk/config.json) \
-  --kiosk \
-  --noerrdialogs \
-  --disable-infobars \
-  --no-first-run \
-  --ozone-platform=wayland \
-  --enable-features=OverlayScrollbar \
-  --start-maximized
+#!/bin/bash
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+    chromium-browser \
+      $(jq -r '.urls | map(.url) | join(" ")' ~/piosk/config.json) \
+      --kiosk \
+      --noerrdialogs \
+      --disable-infobars \
+      --no-first-run \
+      --ozone-platform=wayland \
+      --enable-features=OverlayScrollbar \
+      --start-maximized
+else
+    chromium-browser \
+      $(jq -r '.urls | map(.url) | join(" ")' ~/piosk/config.json) \
+      --kiosk \
+      --noerrdialogs \
+      --disable-infobars \
+      --no-first-run \
+      --enable-features=OverlayScrollbar \
+      --start-maximized
+fi
